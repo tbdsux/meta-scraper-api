1c36987e1608d8072b821e42222445a81e22fc5d
TheBoringDude <iamcoderx@gmail.com>
2021-07-05 10:54:18 +0800

    feat: implement cors middleware

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from typing import Optional
 
 from handler import main_handler
 
 app = FastAPI()
+
+# setup cors middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/", tags=["index"])
